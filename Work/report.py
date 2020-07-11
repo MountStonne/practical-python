91bbaa38fada0150604966d4689ea561f484d740
Shuo Wang <shuow@mun.ca>
2020-07-11 13:47:05 -0230

    in-class practive

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,17 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def portfolio_cost(filename):
+    '''Read in the holding from a portfolio'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (wor[0], int(row[1]), float(wor[2]))
+            portfolio.append(holding)
+    return portfolio
