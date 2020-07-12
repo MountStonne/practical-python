4fb06e0e69e53e2c432f21fadb7310510e209fae
Shuo Wang <shuow@mun.ca>
2020-07-12 21:01:33 -0230

    Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,28 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.27 & 1.30
 
-cost = 0
-with open('/Users/shuowang/practical-python/Work/Data/portfolio.csv', 'rt') as portfolio:
-    headers = next(portfolio)
-    for line in portfolio:
-        row = line.split(',')
-        shares = float(row[1])
-        price = float(row[2])
-        cost += shares * price
+# cost = 0
+# with open('/Users/shuowang/practical-python/Work/Data/portfolio.csv', 'rt') as portfolio:
+#     headers = next(portfolio)
+#     for line in portfolio:
+#         row = line.split(',')
+#         shares = float(row[1])
+#         price = float(row[2])
+#         cost += shares * price
 
-print('Total cost ', cost)
+# print('Total cost ', cost)
+
+def portfolio_cost(filename):
+    '''
+    Returns the total cost of portfolio
+    '''
+    cost = 0
+    with open('/Users/shuowang/practical-python/Work/Data/' + str(filename), 'rt') as portfolio:
+       headers = next(portfolio)
+       for line in portfolio:
+           row = line.split(',')
+           shares = float(row[1])
+           price = float(row[2])
+           cost += shares * price
+    print('Total cost', cost)
