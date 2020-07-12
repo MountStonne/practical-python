605298a2e042f4bacfb9834d2ecf5e976266689e
Shuo Wang <shuow@mun.ca>
2020-07-12 21:23:38 -0230

    Exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,7 +1,6 @@
 # pcost.py
 #
-# Exercise 1.27 & 1.30
-
+## Exercise 1.27
 # cost = 0
 # with open('/Users/shuowang/practical-python/Work/Data/portfolio.csv', 'rt') as portfolio:
 #     headers = next(portfolio)
@@ -13,20 +12,37 @@
 
 # print('Total cost ', cost)
 
+# Exercise 1.31
+# def portfolio_cost(filename):
+#     '''
+#     Returns the total cost of portfolio
+#     '''
+#     cost = 0
+#     with open('/Users/shuowang/practical-python/Work/Data/' + str(filename), 'rt') as portfolio:
+#         headers = next(portfolio)
+#         for line in portfolio:
+#             try:
+#                 row = line.split(',')
+#                 shares = float(row[1])
+#                 price = float(row[2])
+#                 cost += shares * price
+#             except ValueError:
+#                 print('Bad row:',row)
+    
+#     print('Total cost', cost)
+
+## Exercise 1.32
+import csv
 def portfolio_cost(filename):
-    '''
-    Returns the total cost of portfolio
-    '''
     cost = 0
     with open('/Users/shuowang/practical-python/Work/Data/' + str(filename), 'rt') as portfolio:
-        headers = next(portfolio)
-        for line in portfolio:
+        rows = csv.reader(portfolio)
+        headers = next(rows)
+        for row in rows:
             try:
-                row = line.split(',')
                 shares = float(row[1])
                 price = float(row[2])
                 cost += shares * price
             except ValueError:
                 print('Bad row:',row)
-    
     print('Total cost', cost)
