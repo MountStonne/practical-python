a1874aa410b84d15b19b0f5a375d6124bc8a547f
Shuo Wang <shuow@mun.ca>
2020-07-13 11:13:59 -0230

    Exercise 2.7

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.7
 
 import csv
 
@@ -12,6 +12,38 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {
+                'name' : row[0],
+                'shares' : int(row[1]),
+                'price' : float(row[2])
+            }
             portfolio.append(holding)
     return portfolio
+
+def read_prices(filename):
+    '''Read the prices into a dict'''
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices    = read_prices('Data/prices.csv')
+
+cost = 0.0
+for a in portfolio:
+    cost += a['shares']*a['price']
+
+print('Total cost', cost)
+
+value = 0.0
+for b in portfolio:
+    value += b['shares']*prices[b['name']]
+
+print('Current value', value)
+print('Gain', value - cost)
